be40e9b84eea2079fccb98dac26138c0f03ad969
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-06-02 12:24:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytgcalls import PyTgCalls
 from pytgcalls import idle as pyidle
 from config import bot, call_py
 
-
+bot.start()
 print("Ankit VoiceChat Music UserBot Started")
 call_py.start()
 print("Ankit Vc Client Started")
